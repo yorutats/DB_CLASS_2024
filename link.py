e92ada51ea7c346cb8eabe499a8bd7becd5143cf
FangYu <fanny123456789@kimo.com>
2023-03-21 13:23:46 +0800

    hide connection info

--- link.py
+++ link.py
@@ -1,4 +1,4 @@
 import cx_Oracle
 cx_Oracle.init_oracle_client(lib_dir="./instantclient_19_8") # init Oracle instant client 位置
-connection = cx_Oracle.connect('ebookstore', 'ebookstore2022', cx_Oracle.makedsn('140.117.69.70', 1521, service_name='ORCLPDB1'))
+connection = cx_Oracle.connect('account', 'password', cx_Oracle.makedsn('140.117.69.70', 1521, service_name='ORCLPDB1'))
 cursor = connection.cursor()
