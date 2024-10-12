f87255f397a0169ac39be856c614e5949be2028c
howdy <garty6632@gmail.com>
2024-10-12 12:47:33 +0800

    update DB_2024

--- link.py
+++ link.py
@@ -1,11 +1,11 @@
 import psycopg2
 
 connection = psycopg2.connect(
-    user='db_ta',
-    password='nsysu@3024',
+    user='your_account',
+    password='password',
     host='140.117.68.66',
     port='5432',
-    dbname='postgres'  # PostgreSQL 的資料庫名稱
+    dbname='DB_name'  # PostgreSQL 的資料庫名稱
 )
 cursor = connection.cursor()
 
